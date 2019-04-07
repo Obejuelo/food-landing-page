6d553e3685338392f579472726b92f92fd7de4e5
Obed <obejuela4@gmail.com>
2019-04-07 14:01:18 -0500

    Create first step of nav

---
+++ src/components/nav/Nav.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './nav.css';
+
+import { bubble as Menu } from 'react-burger-menu'
+
+export default function () {
+
+    return (
+        <div className="nav">
+            <Menu customBurgerIcon={<img src="assets/icons/menu.png" alt='' />}>
+                <a className="menu-item" href="/">Home</a>
+                <a className="menu-item" href="/burgers">Burgers</a>
+                <a className="menu-item" href="/pizzas">Pizzas</a>
+                <a className="menu-item" href="/desserts">Desserts</a>
+            </Menu>
+            <div className='sign-container'>
+                <span className='icon'>icon</span>
+                <button className='btn btn-sign btn-sm'>Sign In</button>
+            </div>
+        </div>
+    );
+}
